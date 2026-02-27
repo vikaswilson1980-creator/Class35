052e2452bd4c1eb3587bc62458dcc1afe86ded23
vikaswilson1980-creator <vikaswilson1980@gmail.com>
2026-02-27 20:54:48 +0530

    Add files via upload

---
+++ bouncy2.py
@@ -0,0 +1,87 @@
+import pygame
+import random
+pygame.init()
+SPRITE_COLOR_CHANGE_EVENT = pygame.USEREVENT + 1
+BACKGROUND_COLOR_CHANGE_EVENT = pygame.USEREVENT + 2
+BLUE = pygame.Color("blue")
+LIGHT_BLUE = pygame.Color("lightblue")
+DARK_BLUE = pygame.Color("darkblue")
+
+YELLOW = pygame.Color("yellow")
+MAGENTA = pygame.Color("magenta")
+ORANGE = pygame.Color("orange")
+WHITE = pygame.Color("white")
+class Sprite(pygame.sprite.Sprite):
+    def __init__(self,color, height, width):
+        super().__init__()
+        self.image = pygame.Surface([width,height])
+        self.image.fill(color) 
+        self.rect = self.image.get_rect()
+        self.velocity = [random.choice([-1,1]),random.choice([-1,1])]
+    def update(self):
+        self.rect.move_ip(self.velocity)
+        boundry_hit = False
+        if self.rect.left <= 0 or self.rect.right >=500:
+            self.velocity[0] = -self.velocity[0]
+            boundry_hit = True
+        if self.rect.top <= 0 or self.rect.bottom >=400:
+            self.velocity[1] = -self.velocity[1]
+            boundry_hit = True
+        if boundry_hit:
+            pygame.event.post(pygame.event.Event(SPRITE_COLOR_CHANGE_EVENT))
+            pygame.event.post(pygame.event.Event(BACKGROUND_COLOR_CHANGE_EVENT))
+    def change_color(self):
+        self.image.fill(random.choice([YELLOW,MAGENTA,ORANGE,WHITE]))
+def change_background_color():
+    global bgcolor
+    bgcolor = random.choice([BLUE,LIGHT_BLUE,DARK_BLUE])
+class Sprite2(pygame.sprite.Sprite):
+    def __init__(self,color, height, width):
+        super().__init__()
+        self.image = pygame.Surface([width,height])
+        self.image.fill(color) 
+        self.rect = self.image.get_rect()
+        self.velocity = [random.choice([-1,1]),random.choice([-1,1])]
+    def update(self):
+        self.rect.move_ip(self.velocity)
+        boundry_hit = False
+        if self.rect.left <= 0 or self.rect.right >=500:
+            self.velocity[0] = -self.velocity[0]
+            boundry_hit = True
+        if self.rect.top <= 0 or self.rect.bottom >=400:
+            self.velocity[1] = -self.velocity[1]
+            boundry_hit = True
+        if boundry_hit:
+            pygame.event.post(pygame.event.Event(SPRITE_COLOR_CHANGE_EVENT))
+            pygame.event.post(pygame.event.Event(BACKGROUND_COLOR_CHANGE_EVENT))
+    def change_color(self):
+        self.image.fill(random.choice([YELLOW,MAGENTA,ORANGE,WHITE]))
+allsprite_list = pygame.sprite.Group()
+sp1 = Sprite(WHITE,20,30)
+sp1.rect.x = random.randint(0,480)
+sp1.rect.y = random.randint(0,370)
+allsprite_list.add(sp1)
+sp2 = Sprite2(WHITE,20,30)
+sp2.rect.x = random.randint(0,480)
+sp2.rect.y = random.randint(0,370)
+allsprite_list.add(sp2)
+screen = pygame.display.set_mode((500,400))
+pygame.display.set_caption("Boundary Sprite")
+bgcolor = BLUE
+screen.fill(bgcolor)
+exit = False
+clock = pygame.time.Clock()
+while not exit:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit = True
+        elif event.type == SPRITE_COLOR_CHANGE_EVENT:
+            sp1.change_color()
+        elif event.type == BACKGROUND_COLOR_CHANGE_EVENT:
+            change_background_color()
+allsprite_list.update()
+screen.fill(bgcolor)
+allsprite_list.draw(screen)
+pygame.display.flip()
+clock.tick(240)
+pygame.quit()
